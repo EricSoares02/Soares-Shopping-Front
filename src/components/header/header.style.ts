1d4cd1bc675d6352fbb74cce955f090f1c7b2cbf
Eric Soares <ericsoareslopesdev@gmail.com>
2023-12-19 15:38:51 -0300

    feat: add header component

---
+++ src/components/header/header.style.ts
@@ -0,0 +1,16 @@
+import styled from "styled-components"
+
+const HeaderComponent = styled.header`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  height: 75px;
+  z-index: 1000;
+  display: flex;
+  justify-content: center;
+  background-color: ${(props) => props.theme.colors.yellow};
+  box-shadow: 0 0 14px ${(props) => props.theme.colors.shadow};
+`
+
+export {HeaderComponent}
---
+++ src/components/header/header.tsx
@@ -0,0 +1,11 @@
+"use client";
+import { HeaderComponent } from "./header.style";
+import Logo from "./logo/logo";
+
+export default function Header() {
+  return (
+    <HeaderComponent>
+      <Logo />
+    </HeaderComponent>
+  );
+}
