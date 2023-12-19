b8c1774b3bf84a436ae353b71e748fb485a258ef
Eric Soares <ericsoareslopesdev@gmail.com>
2023-12-19 11:29:40 -0300

    feat: add global style and themes

---
+++ src/app/global.style.ts
@@ -0,0 +1,13 @@
+const StylesAndThemes = {
+  colors: {
+
+  },
+  transition: "1.2s",
+  lengths: {
+    
+  },
+  fontStyles: {
+    segoe: "Segoe UI, Tahoma, Geneva, Verdana, sans-serif",
+  },
+};
+export { StylesAndThemes };
