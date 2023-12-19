6478495fd557567c1f3e0a216825a5fe3cf25fe1
Eric Soares <ericsoareslopesdev@gmail.com>
2023-12-19 11:21:40 -0300

    feat: add Global style

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,21 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import { ThemeProvider, createGlobalStyle } from 'styled-components' 
+
+const GlobalStyle = createGlobalStyle`
+* {
+  box-sizing: border-box;
+  padding: 0;
+  margin: 0;
+}
+
+html,
+body {
+  max-width: 100vw;
+  overflow-x: hidden;
+}
+`
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +31,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}><GlobalStyle/>{children}</body>
     </html>
   )
 }
