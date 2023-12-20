7ce11d9d5b7617a01026f94dfa008fb68651af5a
Eric Soares <ericsoareslopesdev@gmail.com>
2023-12-20 14:43:17 -0300

    feat: add slider in home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
+'use client'
 import Image from 'next/image'
 import styles from './page.module.css'
+import Slider from '@/components/home/slider/slider'
 
 export default function Home() {
   return (
     <main>
-      Is Running
+      <Slider></Slider>
     </main>
   )
 }
