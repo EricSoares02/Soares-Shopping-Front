fae6192da540d4e8df294a015752fe6d3d79b4d0
Eric Soares <ericsoareslopesdev@gmail.com>
2023-12-26 11:09:19 -0300

    feat: add separetor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,20 @@
 'use client'
+import Categories from '@/components/home/categories/categories'
 import Slider from '@/components/home/slider/slider'
+import styled from 'styled-components'
+
+const Separetor = styled.div`
+width: 100%;
+height: 50px;
+`
+
 
 export default function Home() {
   return (
     <main>
       <Slider></Slider>
+      <Separetor/>
+      <Categories></Categories>
     </main>
   )
 }
