8494ad1147c8da56e6394d35268f642b95f13557
Eric Soares <ericsoareslopesdev@gmail.com>
2023-12-26 11:08:49 -0300

    feat: add new colors

--- src/app/global.style.ts
+++ src/app/global.style.ts
@@ -4,7 +4,9 @@ const StylesAndThemes = {
     yellow: '#FFF953',
     blue: '#1E6FD9',
     blackBlue: "#033E8C",
-    bg: '#F2F2F2'
+    bg: '#F2F2F2',
+    gray: '#D9D9D9',
+    white: '#FFFFFF'
   },
   transition: "1.2s",
   lengths: {
