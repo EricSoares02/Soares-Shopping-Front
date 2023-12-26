2bc9b5ac5b276e1fd4b1cbfdaa33fd5430d8de6a
Eric Soares <ericsoareslopesdev@gmail.com>
2023-12-26 11:09:50 -0300

    feat: add categories component

---
+++ src/components/home/categories/categories.style.ts
@@ -0,0 +1,46 @@
+import styled from "styled-components"
+
+const CategoriesComponent = {
+
+Conteiner: styled.section`
+    width: 100%;
+    height: 200px;
+    display: flex;
+    justify-content: center;
+`,
+
+CategoriesConteiner: styled.div`
+  width: 70vw;
+  height: 100%;
+  margin: 20px;
+`,
+
+CategoryCase: styled.div`
+  width: 150px;
+  height: 150px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  box-shadow: 0 0 2px ${(props) => props.theme.colors.shadow};
+  transition: ${(props) => props.theme.transition};
+  background-color: ${(props) => props.theme.colors.bg};
+  &:hover {
+    width: 155px;
+    height: 155px;
+  }
+`,
+
+CategoryImg: styled.img`
+  width: 48px;
+  height: 48px;
+`,
+
+ CategoryTitle: styled.p`
+  font-weight: 400;
+  font-size: 14px;
+`
+}
+
+
+export {CategoriesComponent}
---
+++ src/components/home/categories/categories.tsx
@@ -0,0 +1,86 @@
+import { CategoriesComponent } from "./categories.style";
+// import Swiper core and required modules
+import { Navigation, Pagination, Scrollbar, A11y } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+
+
+
+
+export default function Categories() {
+  const CategoriesArray = [
+    {
+      title: "Esporte",
+      img: "categories/ball-category.svg",
+      imgHover: "ball-categoryHover.svg",
+    },
+    {
+      title: "Moda",
+      img: "categories/roupas-category.svg",
+      imgHover: "roupas-categoryHover.svg",
+    },
+    {
+      title: "Informática",
+      img: "categories/informatica-category.svg",
+      imgHover: "informatica-categoryHover.svg",
+    },
+    {
+      title: "Celulares",
+      img: "categories/phone-category.svg",
+      imgHover: "phone-categoryHover.svg",
+    },
+    {
+      title: "Bebidas",
+      img: "categories/bebidas-category.svg",
+      imgHover: "bebidas-categoryHover.svg",
+    },
+    {
+      title: "Eletrodomésticos",
+      img: "categories/eletrodomestico-category.svg",
+      imgHover: "eletrodomestico-categoryHover.svg",
+    },
+    {
+      title: "Ferramentas",
+      img: "categories/ferramentas-category.svg",
+      imgHover: "ferramentas-categoryHover.svg",
+    },
+    {
+      title: "Brinquedos",
+      img: "categories/toy-category.svg",
+      imgHover: "toy-categoryHover.svg",
+    },
+    {
+      title: "Automóveis",
+      img: "categories/volante-category.svg",
+      imgHover: "volante-categoryHover.svg",
+    },
+  ];
+  return (
+    <CategoriesComponent.Conteiner>
+      <CategoriesComponent.CategoriesConteiner>
+      <Swiper
+          // install Swiper modules
+          modules={[Navigation, Pagination, Scrollbar, A11y]}
+          slidesPerView={8}
+          navigation
+          pagination={{ clickable: true }}  
+        >
+          {CategoriesArray.map((category)=>(
+            <SwiperSlide key={category.title}>
+              <CategoriesComponent.CategoryCase>
+                <CategoriesComponent.CategoryImg src={category.img}/>
+                <CategoriesComponent.CategoryTitle>
+                  {category.title}
+                </CategoriesComponent.CategoryTitle>
+              </CategoriesComponent.CategoryCase>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </CategoriesComponent.CategoriesConteiner>
+    </CategoriesComponent.Conteiner>
+  );
+}
