83ef5bba9cb83ccfdf3a4e3f1eab88db91f05c75
Eric Soares <ericsoareslopesdev@gmail.com>
2023-12-26 14:50:45 -0300

    feat: add default product type

---
+++ src/types/products/productType.ts
@@ -0,0 +1,14 @@
+
+export type DefaultProductType = {
+
+id: string, 
+name: string,
+url_img: string[],
+price_in_cent: number,
+desc?: string,
+category: string,
+subCategory: string,
+options: string[],
+storeId: string,
+
+} 
