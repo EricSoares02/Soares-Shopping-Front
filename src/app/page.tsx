0d83855b3fcebe233e855397b07a669020fe711a
Eric Soares <ericsoareslopesdev@gmail.com>
2023-12-26 21:43:57 -0300

    feat: add useProductsRequest

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 'use client'
 import Categories from '@/components/home/categories/categories'
+import Products from '@/components/home/product/product'
 import Slider from '@/components/home/slider/slider'
+import { useProductsRequest } from '@/hooks/hookForHome/useRequestToHome'
 import styled from 'styled-components'
 
 const Separetor = styled.div`
@@ -10,11 +12,15 @@ height: 50px;
 
 
 export default function Home() {
-  return (
+
+  const {data, isLoading} = useProductsRequest()
+  
+  return isLoading ? (<main><h1>carregando</h1></main>) : (
     <main>
       <Slider></Slider>
       <Separetor/>
       <Categories></Categories>
+      {Products(data?.data)}
     </main>
   )
 }
