a2f09ee446489e617d83cb6e8cd905ac39a0726f
Eric Soares <ericsoareslopesdev@gmail.com>
2023-12-26 21:53:18 -0300

    feat: add loading in the home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import Categories from '@/components/home/categories/categories'
 import Products from '@/components/home/product/product'
 import Slider from '@/components/home/slider/slider'
 import { useProductsRequest } from '@/hooks/hookForHome/useRequestToHome'
+import Load from '@/utils/loading/loading'
 import styled from 'styled-components'
 
 const Separetor = styled.div`
@@ -15,7 +16,7 @@ export default function Home() {
 
   const {data, isLoading} = useProductsRequest()
   
-  return isLoading ? (<main><h1>carregando</h1></main>) : (
+  return isLoading ? (<main><Load/></main>) : (
     <main>
       <Slider></Slider>
       <Separetor/>
