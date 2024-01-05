accc523ffc754391d2e73fafb9fdedc2a143fe0c
Eric Soares <ericsoareslopesdev@gmail.com>
2024-01-05 09:31:54 -0300

    refactor: change style

--- src/utils/stars/star.style.ts
+++ src/utils/stars/star.style.ts
@@ -3,6 +3,8 @@ import styled from "styled-components";
 const StarComponent = styled.div<{ $starSize?: number; }>`
 width: ${props=>props.$starSize? props.$starSize : 20}px;
 height: ${props=>props.$starSize? props.$starSize : 20}px;
+display: flex;
+align-items: center;
 & svg{
     width: 100%;
     height: 100%;
