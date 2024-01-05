2e6a9b9eeea41c7e8939e58992018052224a04c0
Eric Soares <ericsoareslopesdev@gmail.com>
2024-01-05 08:23:42 -0300

    feat: add details component

---
+++ src/components/pages/product/productDetails/details.style.ts
@@ -0,0 +1,93 @@
+import { devices } from "@/app/global.screen";
+import styled from "styled-components";
+
+const DetailsComponent = {
+
+  Conteiner: styled.section`
+    width: 400px;
+    height: 800px;
+    display: flex;
+    flex-direction: column;
+    border: solid 1px ${(props) => props.theme.colors.shadowcolor};
+    border-radius: 10px;
+    padding: 20px 0 0 15px;
+    & i {
+      color: ${(props) => props.theme.colors.shadowcolor};
+      text-decoration: line-through;
+      font-family: ${(props) => props.theme.fontStyle};
+    }
+    & span {
+      font-weight: 100;
+      font-size: 50px;
+      font-family: ${(props) => props.theme.fontStyle};
+      color: ${(props) => props.theme.colors.blackBlue};
+    }
+    @media ${devices.lg} {
+      width: 400px;
+    }
+    @media ${devices.sm} {
+      width: 100%;
+    }
+  `,
+
+  ProductName: styled.div`
+    display: flex;
+    align-items: center;
+    width: 100%;
+    height: 50px;
+    & h1 {
+      font-size: 18px;
+    }
+  `,
+
+  TextConteiner: styled.section`
+    width: 100%;
+    height: 256px;
+    padding-top: 8px;
+    p {
+      font-size: 14px;
+    }
+  `,
+  Text: styled.h2`
+    color: #00a650;
+    font-size: 46px;
+    font-family: "Trebuchet MS", "Lucida Sans Unicode", "Lucida Grande",
+      "Lucida Sans", Arial, sans-serif;
+  `,
+
+  //options case
+  OptionsConteiner: styled.section`
+    width: 100%;
+    height: 280px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+
+    h3 {
+      font-size: 16px;
+    }
+  `,
+
+  SelectOptions: styled.select`
+    width: 40%;
+    height: 40px;
+  `,
+
+  Options: styled.option`
+    font-weight: 400;
+    font-size: 16px;
+  `,
+
+  BtnConteiner: styled.div`
+    width: 100%;
+    height: auto;
+    position: relative;
+    bottom: 10px;
+    display: flex;
+    flex-direction: column;
+    padding-left: 35px;
+    gap: 4px;
+  `,
+};
+export { DetailsComponent };
---
+++ src/components/pages/product/productDetails/details.tsx
@@ -0,0 +1,45 @@
+import { converterParaReal } from "@/utils/conversorCoin";
+import { DetailsComponent } from "./details.style";
+
+type PropsType = {
+  props: {
+    name: string;
+    price_in_cent: number;
+    subCategory: string;
+    category: string;
+    options: Array<string>;
+  };
+};
+
+export default function Details(props: PropsType) {
+  return (
+    <DetailsComponent.Conteiner>
+      <DetailsComponent.ProductName>
+        <h1>{props?.props.name}</h1>
+      </DetailsComponent.ProductName>
+      <i>R$50,00</i>
+      <span>{converterParaReal(props.props.price_in_cent)}</span>
+      {/* {StoreCase(data?.productStore)} */}
+      <DetailsComponent.TextConteiner>
+        <p>
+          Categoria: {props.props.category}/{props.props.subCategory}
+        </p>
+        <DetailsComponent.Text>Frete Grátis</DetailsComponent.Text>
+      </DetailsComponent.TextConteiner>
+      <DetailsComponent.OptionsConteiner>
+        <h3>Options:</h3>
+        <DetailsComponent.SelectOptions>
+          {props.props.options.map((item) => (
+            <DetailsComponent.Options key={item} value={item}>
+              {item}
+            </DetailsComponent.Options>
+          ))}
+        </DetailsComponent.SelectOptions>
+      </DetailsComponent.OptionsConteiner>
+      <DetailsComponent.BtnConteiner>
+        {/* {FinishBuyBtn(data.product)}
+        {AddBtnCart(data.product)} */}
+      </DetailsComponent.BtnConteiner>
+    </DetailsComponent.Conteiner>
+  );
+}
