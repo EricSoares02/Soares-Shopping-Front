1cef5742a961543998fdf439158b6a0bcc064ebf
Eric Soares <ericsoareslopesdev@gmail.com>
2024-01-10 15:31:49 -0300

    feat: add show user in header

--- src/components/header/options/options.style.ts
+++ src/components/header/options/options.style.ts
@@ -1,4 +1,4 @@
-import styled, { keyframes } from "styled-components"
+import styled, { keyframes } from "styled-components";
 
 const ping = keyframes`
 75%, 100% {
@@ -8,55 +8,68 @@ const ping = keyframes`
 `;
 
 const OptionsComponent = {
+  DivOptions: styled.div`
+    width: auto;
+    height: 100%;
+    display: flex;
+    gap: 32px;
+    align-items: center;
+    justify-content: end;
+  `,
 
- DivOptions: styled.div`
-  width: auto;
-  height: 100%;
-  display: flex;
-  gap: 32px;
-  align-items: center;
-  justify-content: end;
-`,
+  ImageOptions: styled.img`
+    width: 30px;
+    height: 30px;
+  `,
+  ImageOptionsUser: styled.img`
+    width: 30px;
+    height: 30px;
+  `,
+  ImageOptionUserLogged: styled.div`
+    width: 30px;
+    height: 30px;
+    border-radius: 9999px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: ${(props) => props.theme.colors.white};
+    color: black;
+    border: solid ${(props) => props.theme.colors.gray} 1px;
+    text-decoration: none;
+    text-decoration-line: none;
+    font-size: 13px;
+  `,
 
- ImageOptions: styled.img`
-  width: 30px;
-  height: 30px;
-`,
- ImageOptionsUser: styled.img`
-  width: 30px;
-  height: 30px;
-`,
- AlignToCart: styled.div`
-  width: auto;
-  height: auto;
-  display: flex;
-`,
+  AlignToCart: styled.div`
+    width: auto;
+    height: auto;
+    display: flex;
+  `,
 
- CaseForQuatity: styled.div`
-  width: 10px;
-  height: 10px;
-  translate: -9px 1px;
-`,
- QuatityInCart: styled.div`
-  width: 15px;
-  height: 15px;
-  background-color: ${(props) => props.theme.colors.blue};
-  color: ${(props) => props.theme.colors.white};
-  border-radius: 9999px;
-  font-size: 10px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-`,
- EffectToQuatityInCart: styled.div`
-  width: 15px;
-  height: 15px;
-  background-color: ${(props) => props.theme.colors.blue};
-  border-radius: 9999px;
-  animation: ${ping} 1s cubic-bezier(0, 0, 0.2, 1) infinite;
-  translate: 0px 15px;
-`,
+  CaseForQuatity: styled.div`
+    width: 10px;
+    height: 10px;
+    translate: -9px 1px;
+  `,
+  QuatityInCart: styled.div`
+    width: 15px;
+    height: 15px;
+    background-color: ${(props) => props.theme.colors.blue};
+    color: ${(props) => props.theme.colors.white};
+    border-radius: 9999px;
+    font-size: 10px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  `,
+  EffectToQuatityInCart: styled.div`
+    width: 15px;
+    height: 15px;
+    background-color: ${(props) => props.theme.colors.blue};
+    border-radius: 9999px;
+    animation: ${ping} 1s cubic-bezier(0, 0, 0.2, 1) infinite;
+    translate: 0px 15px;
+  `,
+};
 
-}
-
-export {OptionsComponent}
+export { OptionsComponent };
--- src/components/header/options/options.tsx
+++ src/components/header/options/options.tsx
@@ -1,29 +1,31 @@
-import Link from 'next/link';
-import { OptionsComponent } from './options.style';
+import Link from "next/link";
+import { OptionsComponent } from "./options.style";
 import { useSelector } from "react-redux";
-import { RootStateType } from '@/redux/root-reducer';
+import { RootStateType } from "@/redux/root-reducer";
 
-export default function Options(){
-    
-    const token = useSelector(
-      (state: RootStateType) => state.userSlice
-    );
-  
-    const rout = token.access_token ? '/account' : '/login'
-    const img = '/user.svg'
-    
-return(
+export default function Options() {
+  const token = useSelector((state: RootStateType) => state.userSlice);
+
+  const rout = token.access_token ? "/account" : "/login";
+
+  return (
     <OptionsComponent.DivOptions>
-    <Link href={rout}>
-      <OptionsComponent.ImageOptionsUser src={img} />
-    </Link>
-    <OptionsComponent.AlignToCart>
-      <Link href={'/cart'}>
-        <OptionsComponent.ImageOptions src='/bag.svg' />
+      <Link href={rout}>
+        {token.user_name ?  (
+          <OptionsComponent.ImageOptionUserLogged>
+            {token.user_name.split(" ")[0].charAt(0).toUpperCase() +
+              token.user_name.split(" ")[1].charAt(0).toUpperCase()}
+          </OptionsComponent.ImageOptionUserLogged>
+        ) : (
+            <OptionsComponent.ImageOptionsUser src={"/user.svg"} />
+        )}
       </Link>
-      {3}
-    </OptionsComponent.AlignToCart>
-  </OptionsComponent.DivOptions>
-)
-
+      <OptionsComponent.AlignToCart>
+        <Link href={"/cart"}>
+          <OptionsComponent.ImageOptions src="/bag.svg" />
+        </Link>
+        {3}
+      </OptionsComponent.AlignToCart>
+    </OptionsComponent.DivOptions>
+  );
 }
