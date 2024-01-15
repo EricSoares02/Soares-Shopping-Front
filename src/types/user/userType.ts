001a8488f7c7f66f57dcd146ca7dda862f38e389
Eric Soares <ericsoareslopesdev@gmail.com>
2024-01-15 14:16:11 -0300

    feat: add user default type

---
+++ src/types/user/userType.ts
@@ -0,0 +1,14 @@
+
+
+export type DefaultUserType = {
+
+id: string,
+first_name: string,
+last_name: string,
+url_img?: string,
+email: string,
+password: string,
+role: string,
+storeId?: string,
+
+}
