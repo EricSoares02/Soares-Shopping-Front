6207971937b0a1969a4a9a4dea09ae3c482e76f9
Eric Soares <ericsoareslopesdev@gmail.com>
2024-01-22 08:19:23 -0300

    feat: add cart product type

---
+++ src/types/cart/productType.ts
@@ -0,0 +1,11 @@
+export type CartProductType = {
+    id: string, 
+name: string,
+url_img: string[],
+price_in_cent: number,
+desc?: string,
+category: string,
+subCategory: string,
+options: string,
+storeId: string,
+}
